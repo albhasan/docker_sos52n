c9169db63a86560e7b11ea04bc58f3952f81bf7e
albhasan <albhasan@gmail.com>
2016-06-17 12:13:16 -0300

    added quit output of unzip

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ echo "Get the 52North SOS client..."
 wget http://52north.org/downloads/send/3-sos/484-52n-sensorweb-sos-bundle-4-3-6
 
 echo "Get the SOS WAR and rename it ..."
-unzip 484-52n-sensorweb-sos-bundle-4-3-6
+unzip -q 484-52n-sensorweb-sos-bundle-4-3-6
 mv ./52n-sensorweb-sos-bundle-4.3.6/bin/target/52n-sos-webapp##4.3.6.war ./52n-sos-webapp.war
 
 echo "Start a docker tomcat and load the SOS WAR..."
