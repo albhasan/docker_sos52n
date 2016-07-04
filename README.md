a886c65c7bdd153315507278f5bb8e4d65bea0da
albhasan <albhasan@gmail.com>
2016-07-04 10:44:42 -0300

    instructions improved

--- README.md
+++ README.md
@@ -29,5 +29,18 @@ Scripts for running 52 North's Sensor Observation Service as a docker container.
 <h3>Instructions:</h3>
 <ol>
 	<li>Clone the project and then go to the docker_scidb folder: <code>git clone https://github.com/albhasan/docker_sos52n.git</code></li>
-	<li>Enable <code>setup.sh</code> for execution <code>chmod +x setup.sh</code> and run it <code>./setup.sh</code>. This script downloads 52North's SOS and extracts the <code>SOS WAR</code> file. Then, the script launches a docker Tomcat and it mounts the <code>SOS WAR</code> file into Tomcat's webapps folder. Finally, the SOS server is available at <code>http://localhost:8080/52n-sos-webapp</code>
+	<li>Enable <code>setup.sh</code> for execution <code>chmod +x setup.sh</code> </li>
+	<li>Run <code>setup.sh</code>. This script creates two Docker containers: One with Tomcat and the other with PostGRESQL-PostGIS.</li>
+	<li>Take control of the DB container <code>docker exec -it -u postgres some-postgis /bin/bash</code></li>
+	<li>Create a SQL script in order to create a PostGIS database <code>echo 'CREATE DATABASE sos WITH OWNER = postgres ENCODING = UTF8 TEMPLATE = template_postgis;' >> /tmp/sosdb.sql</code></li>
+	<li>Run the SQL script <code>psql -f /tmp/sosdb.sql</code></li>
+	<li>Get out of the DB container <code>exit</code></li>
+	<li>Finally, the SOS server is available the though the host at <code>http://localhost:8080/52n-sos-webapp/</code>. Go there and follow the SOS's configuration tutorial. You will need the following information:
+		<ul>
+			<li>The database name is <code>sos</code> (see the SQL script).</li>
+			<li>The POSTGRES host IP is diplayed by running <code>docker run -it --link some-postgis:postgres --rm postgres sh -c 'exec echo "$POSTGRES_PORT_5432_TCP_ADDR"'</code> </li>
+			<li>The POSTGRES host port is diplayed by running <code>docker run -it --link some-postgis:postgres --rm postgres sh -c 'exec echo "$POSTGRES_PORT_5432_TCP_PORT"'</code></li>
+			<li>The postgres user password is <code>xxxx.xxxx.xxxx</code></li>
+		</ul>
+	</li>
 </ol>
--- setup.sh
+++ setup.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 echo "#########################################################################"
-echo "LAUNCH A 52 NORTH SENSOR OBSERVATION SERVICE SERVER USING DOCKER"
+echo "SETUP A 52 NORTH SENSOR OBSERVATION SERVICE SERVER USING DOCKER"
 echo "#########################################################################"
 
-echo "Get the 52North SOS client..."
-wget http://52north.org/downloads/send/3-sos/484-52n-sensorweb-sos-bundle-4-3-6
+docker stop some-postgis
+docker rm   some-postgis
+docker stop sos52n
+docker rm   sos52n
 
-echo "Get the SOS WAR and rename it ..."
-unzip -q 484-52n-sensorweb-sos-bundle-4-3-6
-mv ./52n-sensorweb-sos-bundle-4.3.6/bin/target/52n-sos-webapp##4.3.6.war ./52n-sos-webapp.war
 
-echo "Start a docker tomcat and load the SOS WAR..."
+# Get the current folder
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -it --rm -p 8080:8080 -v $DIR/52n-sos-webapp.war:/usr/local/tomcat/webapps/52n-sos-webapp.war tomcat:8.0
+
+echo "Start a PostGRES - PostGIS container..."
+docker run --name some-postgis --expose=5432 -e POSTGRES_PASSWORD=xxxx.xxxx.xxxx -d mdillon/postgis
+
+echo "Start a docker tomcat, and load the SOS WAR, and link the container to PostGIS..."
+docker run --name sos52n -p 8080:8080 --link some-postgis:postgres -v $DIR/52n-sos-webapp.war:/usr/local/tomcat/webapps/52n-sos-webapp.war -d tomcat:8.0
+
+echo "PostGRESQL IP address..."
+docker run -it --link some-postgis:postgres --rm postgres sh -c 'exec echo "$POSTGRES_PORT_5432_TCP_ADDR"'
+
+echo "PostGRESQL IP port..."
+docker run -it --link some-postgis:postgres --rm postgres sh -c 'exec echo "$POSTGRES_PORT_5432_TCP_PORT"'
+
+#echo "#########################################################################"
+# Create a PostGIS database
+#echo "#########################################################################"
+# Take control of the DB container
+#docker exec -it -u postgres some-postgis /bin/bash
+# Create a SQL script
+#echo 'CREATE DATABASE sos WITH OWNER = postgres ENCODING = UTF8 TEMPLATE = template_postgis;' >> /tmp/sosdb.sql
+# Run the SQL script
+#psql -f /tmp/sosdb.sql
 
 echo "Done! - SOS available at http://localhost:8080/52n-sos-webapp/"
